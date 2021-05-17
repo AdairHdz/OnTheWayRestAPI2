0167aaf5486c8c2b1caa833d37f75eee5c5e5882
AdairHdz <adairho16@gmail.com>
2021-05-16 23:34:24 -0500

    chore: Se refactorízó el servicio de login para usar un servicio expuesto en lugar de un controlador de ruta

--- ServicesLayer/services/loginService/LoginService.go
+++ ServicesLayer/services/loginService/LoginService.go
@@ -1,16 +1,76 @@
 package loginService
 
 import (
-	"github.com/AdairHdz/OnTheWayRestAPI/BusinessLayer/businessEntities")
+	"net/http"
+
+	"github.com/AdairHdz/OnTheWayRestAPI/BusinessLayer/businessEntities"
+	"github.com/AdairHdz/OnTheWayRestAPI/helpers/hashing"
+	"github.com/AdairHdz/OnTheWayRestAPI/helpers/tokenGenerator"
+	"github.com/gin-gonic/gin"
+	uuid "github.com/satori/go.uuid"
+)
 
 
 type LoginService struct{}
 
-func (LoginService) Login(emailAddress string) (businessEntities.User, error) {	
-	user := businessEntities.User{
-		EmailAddress: emailAddress,
-	}
+func (LoginService) Login()  gin.HandlerFunc {	
+	return func(context *gin.Context){
+		receivedData := struct {
+			EmailAddress string
+			Password string
+		}{}
 
-	loginError := user.Login()
-	return user, loginError
+		context.BindJSON(&receivedData)
+					
+		user := businessEntities.User{
+			EmailAddress: receivedData.EmailAddress,
+		}
+	
+		loginError := user.Login()			
+
+		if loginError != nil {
+			context.AbortWithStatus(http.StatusConflict)
+			return
+		}
+
+		if user.ID == uuid.Nil {
+			context.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		passwordError := hashing.VerifyPassword(user.Password, receivedData.Password)
+		if passwordError != nil {
+			context.AbortWithStatus(http.StatusUnauthorized)
+			return
+		}
+
+		token, tokenError := tokenGenerator.CreateToken(user.EmailAddress)
+
+		if tokenError != nil {
+			context.Status(http.StatusConflict)
+			return
+		}
+
+		response := struct {
+			ID uuid.UUID
+			Names string
+			LastName string
+			EmailAddress string
+			UserType uint8
+			Verified bool
+			StateID uuid.UUID
+			Token string
+		}{
+			ID: user.ID,
+			Names: user.Names,
+			LastName: user.LastName,
+			EmailAddress: user.EmailAddress,
+			UserType: user.UserType,
+			Verified: user.Verified,
+			StateID: user.StateID,
+			Token: token,
+		}
+
+		context.JSON(http.StatusOK, response)
+	}
 }
